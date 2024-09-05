1c2fab23dfa0f911d02b4e70d2563bad320cec5c
naoki <n.globe.us@gmail.com>
2024-09-05 18:05:23 +0900

    typescript エラーを修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import Home from './App/Home'
 import List from './App/List'
 import AboutUs from './App/AboutUs'
 import Category from './App/Category'
-import Images from './App/Images'
 
 import Tabbar from './App/Tabbar'
 import Papa from 'papaparse'
@@ -113,7 +112,6 @@ const App = () => {
           <Route path="/" element={<Home data={shopList} />} />
           <Route path="/list" element={<List data={shopList} />} />
           <Route path="/category" element={<Category data={shopList} />} />
-          {/* <Route path="/images" element={<Images data={shopList} />} /> */}
           <Route path="/about" element={<AboutUs />} />
         </Routes>
       </div>
