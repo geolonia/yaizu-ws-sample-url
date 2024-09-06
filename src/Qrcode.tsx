ce6ece0998b8629b46e0980023f528c0ba47b0b0
naoki <n.globe.us@gmail.com>
2024-09-07 04:48:33 +0900

    短縮URLを使用

--- src/Qrcode.tsx
+++ src/Qrcode.tsx
@@ -1,19 +1,38 @@
+import { useState, useEffect } from 'react';
 import { useQRCode } from 'next-qrcode';
 
 function Qrcode(props: { url: string }) {
 
   const { url } = props;
   const { Canvas } = useQRCode();
+  const [shortUrl, setShortUrl] = useState<string>('');
+
+  useEffect(() => {
+    fetch('https://s.naoki-is.me', {
+      method: 'POST',
+      body: JSON.stringify({ url })
+    })
+      .then(res => res.json())
+      .then(data => setShortUrl(data.shortUrl))
+      .catch(err => console.error(err))
+  }, [url])
+
+  console.log('shortUrl', shortUrl);
 
   return (
-    <Canvas
-      text={url}
-      options={{
-        type: 'image/png',
-        margin: 0,
-        width: 128
-      }}
-    />
+    <>
+      {
+        shortUrl && (
+          <Canvas
+            text={shortUrl}
+            options={{
+              type: 'image/png',
+              margin: 0,
+              width: 128
+            }}
+          />)
+      }
+    </>
   );
 }
 
